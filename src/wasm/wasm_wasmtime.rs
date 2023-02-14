040dfe37023d6a6bcc820574256efc7cc851d3f9
yanagitani <takanori.yanagitani@gmail.com>
2023-02-15 06:34:09 +0900

    fix: fewer lines per func

--- src/wasm/wasm_wasmtime.rs
+++ src/wasm/wasm_wasmtime.rs
@@ -2,6 +2,59 @@ use wasmtime::{Engine, Instance, Linker, Memory, Module, Store, TypedFunc};
 
 use crate::{err::Error, transform_new};
 
+fn wasm2module(e: &Engine, wasm_bytes: &[u8]) -> Result<Module, Error> {
+    Module::new(e, wasm_bytes)
+        .map_err(|e| Error::UnableToParseWasm(format!("Unable to create a module: {e}")))
+}
+
+fn module2instance<T>(l: &Linker<T>, s: &mut Store<T>, m: &Module) -> Result<Instance, Error> {
+    l.instantiate(s, m)
+        .map_err(|e| Error::UnableToCreateInstance(format!("Link failure: {e}")))
+}
+
+fn mem_find<T>(s: &mut Store<T>, i: &Instance, memname: &str) -> Result<Memory, Error> {
+    i.get_memory(s, memname)
+        .ok_or_else(|| Error::MemoryNotFound(format!("Unable to get a memory(name={memname})")))
+}
+
+fn instance2func<T, I, O>(
+    i: &Instance,
+    s: &mut Store<T>,
+    name: &str,
+) -> Result<TypedFunc<I, O>, Error>
+where
+    I: wasmtime::WasmParams,
+    O: wasmtime::WasmResults,
+{
+    i.get_typed_func(s, name)
+        .map_err(|e| Error::UnableToGetFunc(format!("Unable to get func(name={name}): {e}")))
+}
+
+fn call<T, I, O>(s: &mut Store<T>, f: &TypedFunc<I, O>, args: I) -> Result<O, Error>
+where
+    I: wasmtime::WasmParams,
+    O: wasmtime::WasmResults,
+{
+    f.call(s, args)
+        .map_err(|e| Error::UnableToCallFunc(format!("Unable to call func: {e}")))
+}
+
+fn offset_convert(original: i32) -> Result<usize, Error> {
+    original
+        .try_into()
+        .map_err(|e| Error::InvalidOffset(format!("Unable to convert an offset: {e}")))
+}
+
+fn host2wasm<T>(m: &Memory, s: &mut Store<T>, offset: usize, data: &[u8]) -> Result<(), Error> {
+    m.write(s, offset, data)
+        .map_err(|e| Error::UnableToCopyBytes(format!("writes to mem failed: {e}")))
+}
+
+fn wasm2host<T>(m: &Memory, s: &mut Store<T>, offset: usize, dest: &mut [u8]) -> Result<(), Error> {
+    m.read(s, offset, dest)
+        .map_err(|e| Error::UnableToCopyFromWasm(format!("Unable to read bytes: {e}")))
+}
+
 pub fn transform_trusted_new16(
     wasm_bytes: &[u8],
     mem_name: &str,
@@ -10,51 +63,33 @@ pub fn transform_trusted_new16(
     main_name: &str,
 ) -> Result<impl FnMut([u8; 65536], [u8; 65536]) -> Result<[u8; 65536], Error>, Error> {
     let e: Engine = Engine::default();
-    let m: Module = Module::new(&e, wasm_bytes)
-        .map_err(|e| Error::UnableToParseWasm(format!("Unable to create a module: {e}")))?;
+    let m: Module = wasm2module(&e, wasm_bytes)?;
     let mut s: Store<()> = Store::new(&e, ());
-    let l: Linker<_> = Linker::new(&e);
-    let i: Instance = l
-        .instantiate(&mut s, &m)
-        .map_err(|e| Error::UnableToCreateInstance(format!("Link failure: {e}")))?;
-    let mem: Memory = i
-        .get_memory(&mut s, mem_name)
-        .ok_or_else(|| Error::MemoryNotFound(format!("Unable to get a memory(name={mem_name})")))?;
-    let input_offset_getter: TypedFunc<(), i32> = i
-        .get_typed_func::<(), i32>(&mut s, input_offset_getter_name)
-        .map_err(|e| Error::OffsetFuncMissing(format!("Unable to get a offset func: {e}")))?;
-    let output_offset_getter: TypedFunc<(), i32> = i
-        .get_typed_func::<(), i32>(&mut s, output_offset_getter_name)
-        .map_err(|e| Error::OffsetFuncMissing(format!("Unable to get a offset func: {e}")))?;
-    let main_program: TypedFunc<(i32, i32, i32), i64> = i
-        .get_typed_func::<(i32, i32, i32), i64>(&mut s, main_name)
-        .map_err(|e| Error::MainFuncMissing(format!("Unable to get a main func: {e}")))?;
-    let input_offset: i32 = input_offset_getter
-        .call(&mut s, ())
-        .map_err(|e| Error::UnableToGetOffset(format!("Unable to get offset: {e}")))?;
+    let l: Linker<()> = Linker::new(&e);
+    let i: Instance = module2instance(&l, &mut s, &m)?;
+    let mem: Memory = mem_find(&mut s, &i, mem_name)?;
+    let input_offset_getter: TypedFunc<(), i32> =
+        instance2func(&i, &mut s, input_offset_getter_name)?;
+    let output_offset_getter: TypedFunc<(), i32> =
+        instance2func(&i, &mut s, output_offset_getter_name)?;
+    let main_program: TypedFunc<(i32, i32, i32), i64> = instance2func(&i, &mut s, main_name)?;
+    let input_offset: i32 = call(&mut s, &input_offset_getter, ())?;
     let input_offset4untrusted: i32 = input_offset + 65536;
-    let output_offset: i32 = output_offset_getter
-        .call(&mut s, ())
-        .map_err(|e| Error::UnableToGetOffset(format!("Unable to get offset: {e}")))?;
-    let io_usz: usize = input_offset
-        .try_into()
-        .map_err(|e| Error::InvalidInputOffset(format!("Invalid offset({input_offset}): {e}")))?;
-    let oo_usz: usize = output_offset
-        .try_into()
-        .map_err(|e| Error::InvalidOutputOffset(format!("Invalid offset({output_offset}):{e}")))?;
+    let output_offset: i32 = call(&mut s, &output_offset_getter, ())?;
+    let io_usz: usize = offset_convert(input_offset)?;
+    let oo_usz: usize = offset_convert(output_offset)?;
     let mut buf = [0u8; 65536];
     Ok(
         move |input2trusted: [u8; 65536], output_from_untrusted: [u8; 65536]| {
             let o_input2trusted: usize = io_usz;
             let o_output_from_untrusted: usize = io_usz + 65536;
-            mem.write(&mut s, o_input2trusted, &input2trusted)
-                .map_err(|e| {
-                    Error::UnableToCopyInputBytes(format!("writes to memory failed: {e}"))
-                })?;
-            mem.write(&mut s, o_output_from_untrusted, &output_from_untrusted)
-                .map_err(|e| {
-                    Error::UnableToCopyInputBytes(format!("writes to memory failed: {e}"))
-                })?;
+            host2wasm(&mem, &mut s, o_input2trusted, &input2trusted)?;
+            host2wasm(
+                &mem,
+                &mut s,
+                o_output_from_untrusted,
+                &output_from_untrusted,
+            )?;
             match main_program.call(
                 &mut s,
                 (input_offset, input_offset4untrusted, output_offset),
@@ -65,8 +100,7 @@ pub fn transform_trusted_new16(
                 ))),
                 Err(e) => Err(Error::MainFuncMisbehave(format!("Unexpected error: {e}"))),
             }?;
-            mem.read(&mut s, oo_usz, &mut buf)
-                .map_err(|e| Error::UnableToGetOutputFromTrusted(format!("Unable to read: {e}")))?;
+            wasm2host(&mem, &mut s, oo_usz, &mut buf)?;
             Ok(buf)
         },
     )
